52a77b56cd393d5e15d99f6355b20a174dc080fc
Alvaro Tinoco <62573410+Aetiru@users.noreply.github.com>
2024-08-02 21:50:46 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule } from 'angular-calendar';
+import { DemoComponent } from './calendar/calendar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    FlatpickrModule, // Importa el módulo Flatpickr
+    CalendarModule,  // Importa el módulo Calendar
+    DemoComponent    // Importa el DemoComponent como un componente independiente
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Corrige "styleUrl" a "styleUrls"
 })
 export class AppComponent {
   title = 'calendar';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,27 @@
+import { importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { DemoComponent } from './app/calendar/calendar.component';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      FormsModule,
+      NgbModalModule,
+      FlatpickrModule.forRoot(),
+      CalendarModule.forRoot({
+        provide: DateAdapter,
+        useFactory: adapterFactory,
+      })
+    ),
+  ]
+}).catch(err => console.error(err));
